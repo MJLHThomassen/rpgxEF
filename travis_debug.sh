f51968cd5a820aa8ad6ce7c80de1ea4f9998dbd7
rpgx <m.j.l.h.thomassen@gmail.com>
2017-08-26 20:40:49 +0200

    travis debug

--- travis_debug.sh
+++ travis_debug.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-CMD_PREFIX="amd64-mingw32msvc x86_64-w64-mingw32";
+CMD_PREFIX="i586-mingw32msvc i686-w64-mingw32";
 
 if [ "X$CC" = "X" ]; then
     for check in $CMD_PREFIX; do
@@ -33,7 +33,7 @@ if [ "X$WINDRES" = "X" -o "X$CC" = "X" ]; then
 fi
 
 export PLATFORM=mingw32
-export ARCH=x64
+export ARCH=x86
 
 #exec make $*
 echo "$PATH"
